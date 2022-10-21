2f6e033d5ca1964533befc582c08bbfc34f0afbb
ycen <yycen@live.com>
2022-10-21 13:08:05 +0800

    Fix race condition in test of Eddsa and Shchnorr keygen

--- test/utils.go
+++ test/utils.go
@@ -7,10 +7,35 @@
 package test
 
 import (
+	"sync"
+
 	"github.com/Safulet/tss-lib-private/tss"
 )
 
 func SharedPartyUpdater(party tss.Party, msg tss.Message, errCh chan<- *tss.Error) {
+	// do not send a message from this party back to itself
+	if party.PartyID() == msg.GetFrom() {
+		return
+	}
+	bz, _, err := msg.WireBytes()
+	if err != nil {
+		errCh <- party.WrapError(err)
+		return
+	}
+	pMsg, err := tss.ParseWireMessage(bz, msg.GetFrom(), msg.IsBroadcast())
+	if err != nil {
+		errCh <- party.WrapError(err)
+		return
+	}
+	if _, err := party.Update(pMsg); err != nil {
+		errCh <- err
+	}
+}
+
+func SharedPartyUpdaterWithWg(party tss.Party, msg tss.Message, errCh chan<- *tss.Error, wg *sync.WaitGroup) {
+	if wg != nil {
+		defer wg.Done()
+	}
 	// do not send a message from this party back to itself
 	if party.PartyID() == msg.GetFrom() {
 		return
