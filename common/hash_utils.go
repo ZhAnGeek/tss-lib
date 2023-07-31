66683107e630d2d7c47d79e7a801a8a5dd441672
ycen <yycen@live.com>
2023-07-31 17:47:26 +0800

    Update RejectSample method

--- common/hash_utils.go
+++ common/hash_utils.go
@@ -7,21 +7,15 @@
 package common
 
 import (
-	"crypto/sha256"
+	"crypto/sha512"
 	"math/big"
 )
 
 // RejectionSample implements the rejection sampling logic for converting a
 // SHA512/256 hash to a value between 0-q
 func RejectionSample(q *big.Int, eHash *big.Int) *big.Int { // e' = eHash
-	auxiliary := new(big.Int).Set(eHash)
-	e := new(big.Int).Set(q)
-	one := new(big.Int).SetInt64(1)
-	for e.Cmp(q) != -1 {
-		eHashAdded := auxiliary.Add(auxiliary, one)
-		eHashReSample := sha256.Sum256(eHashAdded.Bytes())
-		// sample 256 bits
-		e = new(big.Int).SetBytes(eHashReSample[:32])
-	}
+	reSampleBytes := sha512.Sum512(append([]byte("RejectSample"), eHash.Bytes()...))
+	eHashReSample := new(big.Int).SetBytes(reSampleBytes[:])
+	e := eHashReSample.Mod(eHashReSample, q)
 	return e
 }
