3e364ccbaba0715d26f56111533b85c9c23d260e
ZhAnGeek <lecky.z@nodereal.io>
2023-08-03 11:55:04 +0800

    fix: Rejection sample v2

--- common/hash_utils.go
+++ common/hash_utils.go
@@ -7,15 +7,39 @@
 package common
 
 import (
-	"crypto/sha512"
+	"crypto/sha256"
 	"math/big"
 )
 
-// RejectionSample implements the rejection sampling logic for converting a
-// SHA512/256 hash to a value between 0-q
-func RejectionSample(q *big.Int, eHash *big.Int) *big.Int { // e' = eHash
-	reSampleBytes := sha512.Sum512(append([]byte("RejectSample"), eHash.Bytes()...))
-	eHashReSample := new(big.Int).SetBytes(reSampleBytes[:])
-	e := eHashReSample.Mod(eHashReSample, q)
+// RejectionSampleV1 implements the rejection sampling logic for converting a
+// @Deprecated SHA512/256 hash to a value between 0-q
+func RejectionSampleV1(q *big.Int, eHash *big.Int) *big.Int { // e' = eHash
+	auxiliary := new(big.Int).Set(eHash)
+	e := new(big.Int).Set(q)
+	one := new(big.Int).SetInt64(1)
+	for e.Cmp(q) != -1 {
+		eHashAdded := auxiliary.Add(auxiliary, one)
+		eHashReSample := sha256.Sum256(eHashAdded.Bytes())
+		// sample 32 bits
+		e = new(big.Int).SetBytes(eHashReSample[:4])
+	}
 	return e
 }
+
+// RejectionSampleV2 implements the rejection sampling logic for converting a
+// SHA512/256 hash to a value between 0-q
+func RejectionSampleV2(q *big.Int, eHash *big.Int) *big.Int { // e' = eHash
+	auxiliary := new(big.Int).Set(eHash)
+	e := new(big.Int).Set(q)
+	qBytesLen := len(q.Bytes())
+	one := new(big.Int).SetInt64(1)
+	for e.Cmp(q) != -1 {
+		eHashAdded := auxiliary.Add(auxiliary, one)
+		eHashReSample := sha256.Sum256(eHashAdded.Bytes())
+		// sample q bits
+		e = new(big.Int).SetBytes(eHashReSample[:qBytesLen])
+	}
+	return e
+}
+
+var RejectionSample func(q *big.Int, eHash *big.Int) *big.Int = RejectionSampleV2
