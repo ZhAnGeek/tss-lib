8f3f95ab1cf01c5444edf21c7ab93f1cc60758f7
ArthurWang <chihhao.wang007@gmail.com>
2024-05-21 09:52:36 +0800

    ckd

--- kcdsa/signing/local_party_test.go
+++ kcdsa/signing/local_party_test.go
@@ -8,6 +8,7 @@ package signing
 
 import (
 	"context"
+	"crypto/elliptic"
 	"fmt"
 	"math/big"
 	"runtime"
@@ -15,13 +16,15 @@ import (
 	"sync/atomic"
 	"testing"
 
-	"github.com/Safulet/tss-lib-private/log"
-	"github.com/stretchr/testify/assert"
-
 	"github.com/Safulet/tss-lib-private/common"
+	"github.com/Safulet/tss-lib-private/crypto"
+	"github.com/Safulet/tss-lib-private/crypto/ckd"
 	"github.com/Safulet/tss-lib-private/kcdsa/keygen"
+	"github.com/Safulet/tss-lib-private/log"
 	"github.com/Safulet/tss-lib-private/test"
 	"github.com/Safulet/tss-lib-private/tss"
+
+	"github.com/stretchr/testify/assert"
 )
 
 const (
@@ -139,11 +142,19 @@ func TestE2EConcurrent(t *testing.T) {
 	msg := big.NewInt(200).Bytes()
 	// init the parties
 	wg := sync.WaitGroup{}
-	keyDerivation := new(big.Int).SetInt64(10)
+
+	// Derive chlid key vault
+	chainCode := make([]byte, 32)
+	max32b := new(big.Int).Lsh(new(big.Int).SetUint64(1), 256)
+	max32b = new(big.Int).Sub(max32b, new(big.Int).SetUint64(1))
+	common.GetRandomPositiveInt(max32b).FillBytes(chainCode)
+	il, childKey, errorDerivation := DerivingPubkeyFromPath(ctx, keys[0].PubKey, chainCode, []uint32{12, 209, 3}, tss.EC())
+	assert.NoErrorf(t, errorDerivation, "there should not be an error deriving the child public key")
+	keyDerivationDelta := il
 	for i := 0; i < len(signPIDs); i++ {
 		params := tss.NewParameters(tss.Curve25519(), p2pCtx, signPIDs[i], len(signPIDs), threshold, false, 0)
 
-		P := NewLocalParty(msg, params, keys[i], keyDerivation, outCh, endCh).(*LocalParty)
+		P := NewLocalParty(msg, params, keys[i], keyDerivationDelta, outCh, endCh).(*LocalParty)
 		parties = append(parties, P)
 		wg.Add(1)
 		go func(P *LocalParty) {
@@ -181,14 +192,36 @@ signing:
 				go updater(ctx, parties[dest[0].Index], msg, errCh)
 			}
 
-		case <-endCh:
+		case sig := <-endCh:
 			atomic.AddInt32(&ended, 1)
 			if atomic.LoadInt32(&ended) == int32(len(signPIDs)) {
-				// already verified in finalize.go
+				rb := common.ReverseBytes(sig.R)
+				sb := common.ReverseBytes(sig.S)
+				s := new(big.Int).SetBytes(rb)
+				e := new(big.Int).SetBytes(sb)
+				assert.True(t, VerifySig(tss.Curve25519(), ctx, s, e, msg, &childKey.PublicKey))
 				t.Logf("Done. Received save data from %d participants", ended)
 
 				break signing
 			}
 		}
 	}
+}
+
+func DerivingPubkeyFromPath(ctx context.Context, masterPub *crypto.ECPoint, chainCode []byte, path []uint32, ec elliptic.Curve) (*big.Int, *ckd.ExtendedKey, error) {
+	// build ecdsa key pair
+	pk, err := crypto.NewECPoint(ec, masterPub.X(), masterPub.Y())
+	if err != nil {
+		return nil, nil, err
+	}
+
+	extendedParentPk := &ckd.ExtendedKey{
+		PublicKey:  *pk,
+		Depth:      0,
+		ChildIndex: 0,
+		ChainCode:  chainCode[:],
+		ParentFP:   []byte{0x00, 0x00, 0x00, 0x00},
+	}
+
+	return ckd.DeriveChildKeyFromHierarchyForKCDSA(ctx, path, extendedParentPk, ec.Params().N, ec)
 }
